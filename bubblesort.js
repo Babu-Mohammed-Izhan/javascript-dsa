645a5a61b92bdc7a60e3ee825ebfaa9c99939b04
Izhan <babuizhan1234@gmail.com>
2022-05-30 22:00:30 +0530

    completed bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,16 @@
+arr1 = [1, 12, 5, 30, 20, 40, 17, 13, 15, 16];
+
+function bubblesort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubblesort(arr1));
